0d56bf7cae570dd4300f36edbdc95c4a21d71dc8
Jukel Rakesh <100283006+jukelrakesh@users.noreply.github.com>
2024-01-20 22:42:22 +0100

    Add files via upload

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -2,11 +2,20 @@ from torch import nn
 
 
 class Classifier(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self):
         super().__init__()
         """
         [TODO]: the classifier should be implemented by the students and different variations of it can be tested
         in order to understand which is the most performing one """
+        self.classifier= nn. Sequential(
+        nn.Linear(1024, 512),
+        nn.ReLU(),
+        nn.Dropout(0.5),
+        nn.Linear(512, 512),
+        nn.ReLU(),
+        nn.Dropout(0.5),
+        nn.Linear(512, 8))
 
     def forward(self, x):
-        return self.classifier(x), {}
+      return self.classifier(x), {}
+
